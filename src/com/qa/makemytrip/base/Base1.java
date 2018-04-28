2abd05544ee52a241e2a395f1e527d40f8a105f6
Ranjan <akhil5386@gmail.com>
2018-04-28 11:46:55 +0530

    commit

--- src/com/qa/makemytrip/base/Base1.java
+++ src/com/qa/makemytrip/base/Base1.java
@@ -37,124 +37,39 @@ import com.qa.makemytrip.utility.CaptureScreenshot;
 import com.qa.makemytrip.utility.OR;
 import com.qa.makemytrip.utility.ReadCredentials;
 import com.qa.makemytrip.utility.mail;
-
 import jxl.read.biff.BiffException;
 
 
 public class Base1 {
-	WebDriver driver;
-	CaptureScreenshot objCaptureScreenshot;
-	Login objLogin;
-	OR OR;
-	mail objmail;
-	ReadCredentials objReadCredentials;
-	@SuppressWarnings("rawtypes")
-	HashMap hm;
-	String  testCase;
+
+	String testCase;
 	
 	@BeforeSuite
 	public void setUp() throws IOException, BiffException {
-		System.setOut(new PrintStream(new FileOutputStream("Report//OutputReport.txt")));
-		FileUtils.deleteDirectory(new File("Output/Screenshots"));
-		OR = new OR();
-		objReadCredentials = new ReadCredentials();
-		hm = objReadCredentials.hm;
 		testCase = System.getProperty("TestCase");
+		System.out.println(testCase);
 	}
-	
-	@BeforeTest
-	public void setUP(){
-		
-		switch(OR.BROWSER) {
-		case "Firefox":
-			System.setProperty("webdriver.gecko.driver", OR.GECKO);
-			driver = new FirefoxDriver();
-			System.out.print("Suite is running on Firefox");
-			break;
-			
-		case "Chrome":
-			System.setProperty("webdriver.chrome.driver", OR.CHROMEDRIVER);
-			driver = new ChromeDriver();
-			System.out.print("Suite is running on Chrome");
-			break;
-			
-		case "IE":
-			System.setProperty("webdriver.ie.driver", OR.CHROMEDRIVER);
-			driver = new FirefoxDriver();
-			System.out.print("Suite is running on Internet Explorer");
-			break;
-		}
-		
-		
-		
-		
-	}
-	
-	@BeforeClass
-	public void initObj() throws FileNotFoundException, IOException {
-		objCaptureScreenshot = new CaptureScreenshot(driver);
-	}
-	
-	
 	
 	@Test(priority=1)
-	public void launchUrl() {
-		/*if(hm.containsKey("Launch Url") && hm.get("Launch Url").equals("Yes")){
-			driver.get(OR.URL);
-		}
-		else {
-			throw new SkipException("Tese Case is Skipped");
-		}*/
-		if(testCase.contains("Launch Url")) {
-			driver.get(OR.URL);
+	public void m1() {
+		if(testCase.contains("TestCase1")) {
+			System.out.println("TestCase1");
 		}
 	}
 	
 	
 	
 	@Test(priority=2)
-	public void testLogin() throws IOException {
-		/*if(hm.containsKey("Login") && hm.get("Login").equals("Yes")){
-			objLogin = new Login(driver);
-			objLogin.baseLogin(objReadCredentials.username,objReadCredentials.password);
-			objLogin=null;
-		}
-		else {
-			throw new SkipException("Tese Case is Skipped");
-		}*/
-		
-		if(testCase.contains("Login")) {
-			objLogin = new Login(driver);
-			objLogin.baseLogin(objReadCredentials.username,objReadCredentials.password);
-			objLogin=null;
-
+	public void m2() {
+		if(testCase.contains("TestCase2")) {
+			System.out.println("TestCase2");
 		}
 	}
 	
 	@Test(priority=3)
-	public void testLogout() {
-		if(hm.containsKey("Logout") && hm.get("Logout").equals("Yes")){
-			System.out.println("Logout method is passed");
-		}
-		else {
-			throw new SkipException("Tese Case is Skipped");
+	public void m3() {
+		if(testCase.contains("TestCase3")) {
+			System.out.println("TestCase3");
 		}
 	}
-	
-	@SuppressWarnings("static-access")
-	@AfterMethod
-	public void screenShots(ITestResult mResult) throws IOException {
-		if(mResult.FAILURE==mResult.getStatus()) {
-			objCaptureScreenshot.takeScreensot(driver,mResult);
-		}
-			System.gc();
 	}
-	
-	@AfterSuite
-	public void tearDown() throws IOException, AddressException, MessagingException {
-		objmail= new mail();
-		objmail.sendEmail();
-		driver.quit();
-			
-	}
-}
